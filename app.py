22c52051721660511a38c99bda5a56de1931c9cc
Tomasz Jastrzębski <tomasz.jastrzebski95@gmail.com>
2023-11-24 11:32:53 +0100

    v2.0

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+def redirect_on_user_agent(user_agent):
+    if "Android" in user_agent:
+        return redirect('https://play.google.com/store/apps/details?id=pl.ayground.playbackapp&hl=pl&gl=US')
+    elif "Windows" in user_agent:
+        return redirect('https://mp3cut.net/pl/reverse-audio')
+    elif "iPhone" in user_agent:
+        return redirect('https://apps.apple.com/us/app/reverse-audio/id1254981556')
+
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -12,11 +21,11 @@ def index():
 
 @app.route('/check_keyword', methods=['POST'])
 def check_keyword():
-    k = request
     keyword = request.form['keyword']
+    user_agent = request.headers.get('User-Agent')
 
     if keyword.lower() == 'apokalipsa':
-        return redirect('https://mp3cut.net/pl/reverse-audio')
+        redirect_on_user_agent(user_agent)
     else:
         return render_template('index.html', message='Nieprawidłowa odpowiedź!')
 
